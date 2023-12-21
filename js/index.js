0505b0a56f62b836052d4f9e9f1b4ade1971aa7e
Yaroslav <6715414@gmail.com>
2023-12-21 10:04:10 +0200

    feat: Integrate efficientPlacement algorithm into main application

---
+++ js/index.js
@@ -0,0 +1,45 @@
+import blocksData from '../inputs/blocks.json' assert { type: "json" };
+
+import { efficientPlacement } from './algorithms/efficientPlacement.js';
+import Render from './lib/render.js';
+
+import Block from './components/block.js';
+import Container from './components/container.js';
+
+import { getRandomColorBy2Values } from './utils/generateColor.js';
+import { debounce } from './utils/debounce.js';
+
+function main(blocks) {
+  const container = new Container(window.innerWidth, window.innerHeight);
+
+  const containerElement = document.getElementById('container');
+  containerElement.innerHTML = '';
+
+  const data = efficientPlacement(blocks, container);
+
+  data.blockCoordinates.forEach((coord) => {
+    const width = coord.right - coord.left;
+    const height = coord.bottom - coord.top
+
+    const block = new Block(
+      width,
+      height,
+      coord.initialOrder,
+      getRandomColorBy2Values(width, height),
+      { ...coord }
+    );
+
+    Render.renderBlock(block, containerElement);
+  });
+
+  Render.renderFullness(data.fullness);
+}
+
+
+main(blocksData);
+
+window.addEventListener('resize', debounce(() => {
+  main(blocksData);
+}, 50));
+
+
