2776173649b994a0336c6b7ec2a8ae6945d05292
Yaroslav <6715414@gmail.com>
2023-12-21 09:52:43 +0200

    feat: Add Block and Container components

---
+++ js/components/block.js
@@ -0,0 +1,11 @@
+class Block {
+    constructor(width, height, initialOrder, color, coords) {
+        this.width = width;
+        this.height = height;
+        this.initialOrder = initialOrder;
+        this.color = color;
+        this.coords = coords;
+    }
+}
+
+export default Block;
---
+++ js/components/container.js
@@ -0,0 +1,9 @@
+class Container {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+}
+
+export default Container;
+
