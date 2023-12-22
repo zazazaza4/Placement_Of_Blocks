e25c35c24717303ec3dfdee588b5adf17c9e12fd
Yaroslav <6715414@gmail.com>
2023-12-22 17:35:55 +0200

    refactor: loadJson helper

--- js/utils/jsonLoader.js
+++ js/utils/jsonLoader.js
@@ -1,16 +1,10 @@
 export async function loadJson(url) {
-  return new Promise((resolve, reject) => {
-    fetch(url)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`Failed to fetch JSON: ${response.status} ${response.statusText}`);
-        }
-        return response.json();
-      })
-      .then((data) => resolve(data))
-      .catch((error) => {
-        console.error(`Error loading JSON from ${url}:`, error);
-        reject(error);
-      });
-  });
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(`Error loading JSON from ${url}:`, error);
+    throw error;
+  }
 }
