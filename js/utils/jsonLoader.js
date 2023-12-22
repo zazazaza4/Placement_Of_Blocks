2494c2159e1038160beb490196215489d990655c
Yaroslav <6715414@gmail.com>
2023-12-22 12:08:11 +0200

    feat: add loadJson helper

---
+++ js/utils/jsonLoader.js
@@ -0,0 +1,10 @@
+export async function loadJson(url) {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error(`Error loading JSON from ${url}:`, error);
+      throw error;
+    }
+  }
